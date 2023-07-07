af727896c1e10bfd2eb8a27300b0332fc3560e48
Jeff Newman <jeff@newman.me>
2023-07-07 11:23:53 -0500

    from __future__ import annotations

--- simbywire/config/booking_curves.py
+++ simbywire/config/booking_curves.py
@@ -1,10 +1,4 @@
-#                     name = str(z[1]).strip()
-#                     curve = airsim_utils.parse_array(z[2:])
-#                     bc = BookingCurve(name)
-#                     self.curves[name] = bc
-#                     for _dcp, _pct in zip(self.dcps, curve):
-#                         bc.add_dcp(int(_dcp), float(_pct))
-
+from __future__ import annotations
 
 from .named import Named
 
