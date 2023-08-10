bd9d348057608a2dc659dacdaa8b109259a9f4df
Jeff Newman <jeff@newman.me>
2023-08-10 08:32:50 -0500

    documentation scripts

---
+++ docs/.scripts/_execute_notebooks.py
@@ -0,0 +1,40 @@
+"""Execute notebooks."""
+
+import re
+from pathlib import Path
+
+import nbformat
+from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
+
+doc_path = Path(__file__).parents[1]
+
+print(f"executing notebooks found within {doc_path}")
+
+exclusions = ["reading-data"]
+
+for path in sorted(doc_path.rglob("*.ipynb")):
+    if ".ipynb_checkpoints" in path.parts:
+        # skip any checkpoints
+        continue
+    if path.name.endswith(".nbconvert.ipynb"):
+        # this is an execution output, don't nest
+        continue
+    exclude = False
+    for e in exclusions:
+        if re.search(e, str(path)):
+            exclude = True
+            continue
+    if exclude:
+        continue
+
+    print(f"converting {path} to {path.with_suffix('.nbconvert.ipynb')}")
+    with open(path) as f:
+        nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600)
+        try:
+            ep.preprocess(nb, {"metadata": {"path": path.parent}})
+        except CellExecutionError:
+            print('Error executing the notebook "%s".' % path)
+        finally:
+            with open(path.with_suffix(".nbconvert.ipynb"), "w", encoding="utf-8") as f:
+                nbformat.write(nb, f)
