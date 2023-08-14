76d6b935e0318e6c4c76329914da95c0ac566251
Jeff Newman <jeff@newman.me>
2023-08-14 15:42:54 -0500

    tidy

--- simbywire/summary.py
+++ simbywire/summary.py
@@ -38,15 +38,25 @@ class SummaryTables:
             return df
         import altair as alt
 
+        chart = alt.Chart(df)
+        bars = chart.mark_bar().encode(
+            x=alt.X("name:N", title="Airline"),
+            y=alt.Y("value", stack=None, title="miles"),
+            color="measure",
+            tooltip=["name", "measure", alt.Tooltip("value", format=".4s")],
+        )
+        text = chart.mark_text(
+            dx=0,
+            dy=5,
+            color="white",
+            baseline="top",
+        ).encode(
+            x=alt.X("name:N"),
+            y=alt.Y("value").stack(None),
+            text=alt.Text("value:Q", format=".4s"),
+        )
         return (
-            alt.Chart(df)
-            .mark_bar()
-            .encode(
-                x=alt.X("name:N", title="Airline"),
-                y=alt.Y("value:Q", stack=None, title="miles"),
-                color="measure",
-                tooltip=["name", "measure", alt.Tooltip("value", format=".4s")],
-            )
+            (bars + text)
             .properties(
                 width=400,
                 height=300,
@@ -61,25 +71,41 @@ class SummaryTables:
             )
         )
 
-    def fig_fare_class_mix(self, raw_df=False):
+    def fig_fare_class_mix(self, raw_df=False, label_threshold=0.06):
         df = self.fare_class_mix[["carrier", "booking_class", "avg_sold"]]
         if raw_df:
             return df
         import altair as alt
 
+        label_threshold_value = (
+            df.groupby("carrier").avg_sold.sum().max() * label_threshold
+        )
+        chart = alt.Chart(df).transform_calculate(
+            halfsold="datum.avg_sold / 2.0",
+        )
+        bars = chart.mark_bar().encode(
+            x=alt.X("carrier:N", title="Airline"),
+            y=alt.Y("avg_sold:Q", title="Seats").stack("zero"),
+            color="booking_class",
+            tooltip=[
+                "carrier",
+                "booking_class",
+                alt.Tooltip("avg_sold", format=".2f"),
+            ],
+        )
+        text = chart.mark_text(dx=0, dy=3, color="white", baseline="top").encode(
+            x=alt.X("carrier:N", title="Airline"),
+            y=alt.Y("avg_sold:Q", title="Seats").stack("zero"),
+            text=alt.Text("avg_sold:Q", format=".2f"),
+            opacity=alt.condition(
+                f"datum.avg_sold < {label_threshold_value:.3f}",
+                alt.value(0),
+                alt.value(1),
+            ),
+            order=alt.Order("booking_class:N", sort="descending"),
+        )
         return (
-            alt.Chart(df)
-            .mark_bar()
-            .encode(
-                x=alt.X("carrier:N", title="Airline"),
-                y=alt.Y("avg_sold:Q", stack=None, title="Seats"),
-                color="booking_class",
-                tooltip=[
-                    "carrier",
-                    "booking_class",
-                    alt.Tooltip("avg_sold", format=".2f"),
-                ],
-            )
+            (bars + text)
             .properties(
                 width=400,
                 height=300,
